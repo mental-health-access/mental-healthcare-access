2ca9e55adbde034e42aa163b023b9fd00cca653f
Chris Bay <chris@chrisbay.net>
2021-04-22 18:05:16 -0500

    Restructures project and renames main packages and app class

--- src/test/java/org/launchcode/mentalhealthcareaccess/MentalHealthcareAccessApplicationTests.java
+++ src/test/java/org/launchcode/mentalhealthcareaccess/MentalHealthcareAccessApplicationTests.java
@@ -1,10 +1,10 @@
-package org.launchcodeiftoff.hello.world;
+package org.launchcode.mentalhealthcareaccess;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloWorldApplicationTests {
+class MentalHealthcareAccessApplicationTests {
 
 	@Test
 	void contextLoads() {
