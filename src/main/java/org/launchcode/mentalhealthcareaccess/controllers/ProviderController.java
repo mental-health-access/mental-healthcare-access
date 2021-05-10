55357b983201830ea1c9948145106f273dd37cbc
ayamuz <ayamuz@gmail.com>
2021-05-09 19:50:58 -0500

    Takes newProvider to dashboard and shows info. Login still not working

--- src/main/java/org/launchcode/mentalhealthcareaccess/controllers/ProviderController.java
+++ src/main/java/org/launchcode/mentalhealthcareaccess/controllers/ProviderController.java
@@ -2,6 +2,7 @@ package org.launchcode.mentalhealthcareaccess.controllers;
 import org.launchcode.mentalhealthcareaccess.models.data.ProviderRepository;
 import org.launchcode.mentalhealthcareaccess.models.Provider;
 
+import org.launchcode.mentalhealthcareaccess.models.data.dto.LoginFormDTO;
 import org.launchcode.mentalhealthcareaccess.models.data.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,7 +21,6 @@ public class ProviderController {
 
     @Autowired
     ProviderRepository providerRepository;
-
 
     private static final String providerSessionKey = "provider";
 
@@ -44,21 +44,22 @@ public class ProviderController {
         session.setAttribute(providerSessionKey, provider.getId());
     }
 
+
      @GetMapping("provider/signup")
      public String displaySignUpForm(Model model) {
          model.addAttribute(new RegisterFormDTO());
-         model.addAttribute("title", "Register");
-         return "/provider/signup";
+         model.addAttribute("title", "Sign-Up");
+         return "provider/signup";
     }
 
-    @PostMapping("/provider/signup")
+    @PostMapping("provider/signup")
     public String processSignUpForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
                                           Errors errors, HttpServletRequest request,
-                                          Model model) {
+                                          Model model, HttpSession session) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Sign-up");
-            return "/provider/signup";
+            return "provider/signup";
         }
 
         Provider existingProvider = providerRepository.findByEmail(registerFormDTO.getEmail());
@@ -67,20 +68,20 @@ public class ProviderController {
             //This is a custom error
             errors.rejectValue("email", "email.alreadyexists", "This email is already registered, please choose anew one");
             model.addAttribute("title", "Sign-Up");
-            return "/provider/signup";
+            return "provider/signup";
         }
 
         if (registerFormDTO.getCompanyName() == "" && (registerFormDTO.getFirstName() == "" || registerFormDTO.getLastName() == "") ){
             //This is a custom error
             errors.rejectValue("companyName", "companyName.cannotBeBlank", "You must enter a Company Name or a First and Last Name for the provider");
             model.addAttribute("title", "Sign-Up");
-            return "/provider/signup";
+            return "provider/signup";
         }
         if (registerFormDTO.getCompanyName() != "" && (registerFormDTO.getFirstName() != "" || registerFormDTO.getLastName() != "") ){
             //This is a custom error
             errors.rejectValue("companyName", "companyName.onlyOneType", "You must choose to enter either a Company Name or a First and Last Name");
             model.addAttribute("title", "Sign-Up");
-            return "/provider/signup";
+            return "provider/signup";
         }
 
         //This password was set in LoginFormDTO
@@ -90,7 +91,7 @@ public class ProviderController {
         if (!password.equals(verifyPassword)) {
             errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
             model.addAttribute("title", "Sign-up");
-            return "/provider/signup";
+            return "provider/signup";
         }
 
         Provider newProvider = new Provider(registerFormDTO.getDisplayName(),
@@ -104,6 +105,53 @@ public class ProviderController {
         providerRepository.save(newProvider);
 
         setProviderInSession(request.getSession(), newProvider);
-        return "redirect:";
+
+        session.setAttribute("name", newProvider.getDisplayName());
+        session.setAttribute("email", newProvider.getEmail());
+        session.setAttribute("phone", newProvider.getPhoneNumber());
+
+        return "provider/dashboard";
+    }
+
+    @GetMapping("/provider/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "/provider/login";
+    }
+    @PostMapping("/provider/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model, HttpSession session) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Log In");
+            return "/provider/login";
+        }
+        //Find Provider in DB
+        Provider theProvider = providerRepository.findByEmail(loginFormDTO.getEmail());
+
+        //Custom error if this email is not registered
+        if (theProvider == null) {
+            errors.rejectValue("email", "provider.invalid", "This e-mail is not registered");
+            model.addAttribute("title", "Log In");
+            return "/provider/login";
+        }
+
+        //Compare password input to DB
+        String password = loginFormDTO.getPassword();
+        if (!theProvider.isMatchingPassword(password)) {
+            //Error if wrong pass
+            errors.rejectValue("password", "password.invalid", "Incorrect password, please verify");
+            model.addAttribute("title", "Log In");
+            return "/provider/login";
+        }
+        //Create a new session for the provider
+        setProviderInSession(request.getSession(), theProvider);
+        session.setAttribute("name", theProvider.getDisplayName());
+        session.setAttribute("email", theProvider.getEmail());
+        session.setAttribute("phone", theProvider.getPhoneNumber());
+        //Back to dashboard
+        return "dashboard";
     }
 }
